fcf0ee449af96fa63d8936cf31dbb82712e6cf7b
Jose Calderon <josecalderon@Joses-Air.lan>
2021-06-20 15:12:42 +0100

    Added init kafka processor files

---
+++ src/test/java/com/jcalderon/domainprocessor/DomainProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcalderon.domainprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
